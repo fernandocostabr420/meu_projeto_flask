6b601fa9671f6565f3e1724f2a30bb4228f13b39
Fernando Morais <fernando.costabr20@gmail.com>
2024-10-19 16:19:07 -0300

    Configuração para produção usando Gunicorn

--- app.py
+++ app.py
@@ -6,17 +6,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import time
+import os
 
 app = Flask(__name__)
-
-# Função para converter o preço para um valor numérico (float)
-def converter_preco(preco_str):
-    return float(preco_str.replace("R$", "").replace(".", "").replace(",", "."))
 
 # Função para buscar orçamentos usando Selenium
 def buscar_orcamento(data_checkin, data_checkout, numero_adultos, numero_menores=0):
     orcamentos = []
-    total_pessoas = numero_adultos + numero_menores  # Total de pessoas solicitadas
 
     # Caminho correto para o executável do ChromeDriver
     PATH = "C:/webdriver/chromedriver-win64/chromedriver.exe"
@@ -59,19 +55,12 @@ def buscar_orcamento(data_checkin, data_checkout, numero_adultos, numero_menores
                 disponivel = bloco.find_elements(By.CSS_SELECTOR, "button.btn.btn-raised.btn-orange.slim.text-uppercase")
                 pessoas = bloco.find_element(By.CSS_SELECTOR, "label.text-secondary").text.strip()
 
-                # Verificar se o número de pessoas do quarto corresponde ao número solicitado
-                if int(pessoas) != total_pessoas:
-                    print(f"Quarto para {pessoas} pessoas não corresponde ao solicitado ({total_pessoas} pessoas), pulando...")
-                    continue
-
                 if not disponivel:
                     print(f"Quarto para {pessoas} pessoas não está disponível, pulando...")
                     continue
 
                 preco = bloco.find_element(By.CSS_SELECTOR, "span.value.currency").text.strip()
-
-                # Adicionar "pessoas" ao lado do número de pessoas
-                orcamentos.append((pessoas + " pessoas", preco))
+                orcamentos.append((pessoas, preco))
             except Exception as e:
                 print(f"Erro ao processar o bloco de quartos: {e}")
                 continue
@@ -81,11 +70,7 @@ def buscar_orcamento(data_checkin, data_checkout, numero_adultos, numero_menores
     finally:
         driver.quit()
 
-    # Ordenar os orçamentos pelo preço (conversão de string para valor numérico)
-    orcamentos.sort(key=lambda x: converter_preco(x[1]))
-
-    # Retornar apenas o orçamento mais barato
-    return orcamentos[:1]  # Pegando apenas o orçamento mais barato
+    return orcamentos
 
 # Rota para buscar orçamentos
 @app.route('/buscar_orcamento', methods=['POST'])
@@ -104,6 +89,7 @@ def buscar():
     
     return jsonify({'orcamentos': orcamentos})
 
-# Iniciar o servidor Flask
-if __name__ == '__main__':
-    app.run(debug=True)
+# Iniciar o servidor Flask no ambiente de produção
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))  # Usar a variável de ambiente PORT fornecida pela Railway
+    app.run(host="0.0.0.0", port=port)  # Ouvir em todas as interfaces de rede (0.0.0.0)
